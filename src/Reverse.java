806d1f38428e1b0de37596c3c4c7c8ff130807e6
avinashraz <avinashaninthaneni@gmail.com>
2016-11-07 12:40:55 +0530

    Reverse Of A Given String Value

---
+++ src/Reverse.java
@@ -0,0 +1,14 @@
+public class Reverse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		StringBuffer text = new StringBuffer("Print reverse of a given String value.");
+		text.reverse();
+		System.out.println(text);
+		
+		
+
+	}
+
+}
